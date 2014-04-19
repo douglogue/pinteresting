a149773194108d7a5ec77b5171a5d9cf81cd37be
Doug Logue <logue.doug@gmail.com>
2014-04-19 14:01:34 -0400

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Required for Heroko
+    # Note to set this to your actual url
+  config.action_mailer.default_url_options = { host: 'doug-pinteresting.herokuapp.com' }
 end
